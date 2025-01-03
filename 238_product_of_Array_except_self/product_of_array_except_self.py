efaa68f52f319e5a1c3ac52c60c124936d86373a
dawitYenew.12 <dawityenew.12@gmail.com>
2025-01-03 21:31:38 +0300

    product of array except self

---
+++ 238_product_of_Array_except_self/product_of_array_except_self.py
@@ -0,0 +1,13 @@
+class Solution:
+    def productExceptSelf(self, nums):
+        n = len(nums)
+        ans = [1] * n
+        suffix_prod = 1
+        # prefix product
+        for i in range(1, n):
+            ans[i] = ans[i - 1] * nums[i - 1]
+        # suffix product
+        for i in range(n - 1, -1, -1):
+            ans[i] *= suffix_prod
+            suffix_prod *= nums[i]
+        return ans
