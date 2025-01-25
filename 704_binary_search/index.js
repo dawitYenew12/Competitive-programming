1e0c6bc4a40ad1e45d429e9931eae055d9f5523f
dawitYenew.12 <dawityenew.12@gmail.com>
2025-01-25 21:42:36 +0300

    binary search

---
+++ 704_binary_search/index.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let l = 0;
+    let r = nums.length - 1;
+    while (l <= r) {
+        let m = l + Math.floor((r - l) / 2);
+        if (nums[m] === target) {
+            return m;
+        } else if (target < nums[m]){
+            r = m - 1
+        } else {
+            l = m + 1;
+        }
+    }
+    return -1;
+};
