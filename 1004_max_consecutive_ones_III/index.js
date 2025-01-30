3445b31d496b990f9391a9bba8c8c6ff34a5e096
dawitYenew.12 <dawityenew.12@gmail.com>
2025-01-30 10:06:24 +0300

    max consecutive ones

---
+++ 1004_max_consecutive_ones_III/index.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var longestOnes = function (nums, k) {
+    let left = 0;
+    let max_length = 0;
+    let zero_count = 0;
+
+    for (let right = 0; right < nums.length; right++) {
+        if (nums[right] === 0) {
+            zero_count++;
+        }
+
+        while (zero_count > k) {
+            if (nums[left] === 0) {
+                zero_count--;
+            }
+            left++;
+        }
+
+        max_length = Math.max(max_length, right - left + 1);
+    }
+
+    return max_length;
+};
