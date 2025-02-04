722660aa2830aff82a93e76f5f3b8798cb61720a
dawitYenew.12 <dawityenew.12@gmail.com>
2025-02-04 23:07:13 +0300

    maximum subarray form

---
+++ 1800_maximum_subarray_form/index.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxAscendingSum = function(nums) {
+    if (nums.length === 0) return 0;
+
+    let maxSum = 0;
+    let currentSum = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > nums[i - 1]) {
+            currentSum += nums[i];
+        } else {
+            maxSum = Math.max(maxSum, currentSum);
+            currentSum = nums[i];
+        }
+    }
+
+    // Ensure the last subarray's sum is checked
+    maxSum = Math.max(maxSum, currentSum);
+
+    return maxSum;
+};
